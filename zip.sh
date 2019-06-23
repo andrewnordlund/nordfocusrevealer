5f29b81a20af884860bb1f000eefc8b599328bca
Andrew Nordlund <github@nordburg.ca>
2019-06-23 19:58:46 -0400

    Okay now finished cleaning.

--- zip.sh
+++ zip.sh
@@ -1,2 +1,2 @@
 rm nordFocusRevealer-we.xpi
-zip -r nordFocusRevealer-we.xpi LICENSE README.md  manifest.json nordFocusReveal-bg.js content_scripts icons options_pages -x *.swp *.DS_Store
+zip -r nordFocusRevealer-we.xpi LICENSE README.md  manifest.json nordFocusReveal-bg.js libs content_scripts icons options_pages -x *.swp *.DS_Store
